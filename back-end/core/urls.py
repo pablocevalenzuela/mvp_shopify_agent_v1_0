92c155cfcc55723c3a7f60a4b1478e2a88537e79
pablocevalenzuela <pablocevalenzuela@gmail.com>
2026-03-05 12:37:26 -0300

    (fix): solving bug during developing logic Agent AI into a back-end

--- back-end/core/urls.py
+++ back-end/core/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.get_urls()),
+    path('admin/', admin.site.urls),
     path('api/shopify/', include('shopify_agent.urls')),
 ]
 
