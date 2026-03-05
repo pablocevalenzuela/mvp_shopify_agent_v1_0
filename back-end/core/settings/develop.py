eaa47c44ca039d745c2f26fa1d3030f2c4f2ba07
pablocevalenzuela <pablocevalenzuela@gmail.com>
2026-03-05 20:57:14 -0300

    (fix): solving CSRF verification error whit ngrok

--- back-end/core/settings/develop.py
+++ back-end/core/settings/develop.py
@@ -9,6 +9,18 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env.develop'))
 # Sobrescribimos el DEBUG si es necesario
 DEBUG = True
 
+# Permitir el dominio de ngrok para evitar el error 403 CSRF
+CSRF_TRUSTED_ORIGINS = [
+    "https://griffin-chirographic-alfonso.ngrok-free.dev",
+]
+
+# También asegúrate de que esté en ALLOWED_HOSTS (aunque en base.py pusimos '*' por defecto)
+ALLOWED_HOSTS = [
+    "griffin-chirographic-alfonso.ngrok-free.dev",
+    "localhost",
+    "127.0.0.1",
+]
+
 DATABASES = {
     # Lee la variable DATABASE_URL de .env.develop y la configura para Django.
     # Nota: env viene importado desde .base
